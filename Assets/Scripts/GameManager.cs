811c172c63eabf3e1bd6802c618af2e88de7fc0b
Nibiru Imagineering - Lanny "gilbot" Gilbertson <lannygilbertson@protonmail.com>
2024-05-04 02:19:36 +0300

    Final commit
    - Edited Main scene Best Score text (naming it)
    - Added reference on BestScoreText in MainManager Script
    - Instantiate MainManager class
    - Added a call to SetBestScore (from GameManager) in the GameOver() method
    
    - Created  SetBestScore() Method in GameManager
    - Created SaveGameInfo() and LoadGameInfo() methods in the GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 #if UNITY_EDITOR
 using UnityEditor;
+using System.IO;
 #endif
 
 public class GameManager : MonoBehaviour
@@ -18,6 +19,25 @@ public class GameManager : MonoBehaviour
 
     public static GameManager Instance;
 
+    private void Start()
+    {
+        if (bestName != "")
+        {
+            _bestScoreText.text = "Best Score : " + bestName + " : " + bestScore;
+        }
+    }
+
+    public void SetBestScore(int score)
+    {
+        if(score > bestScore) 
+        {
+            bestScore = score;
+            bestName = name;
+            SaveGameInfo();
+            MainManager.Instance.BestScoreText.text = "Best Score : " + bestName + " : " + bestScore; 
+        }
+    }
+
     private void Awake()
     {
         if (Instance != null)
@@ -27,6 +47,7 @@ public class GameManager : MonoBehaviour
 
         Instance = this;
         DontDestroyOnLoad(gameObject);
+        LoadGameInfo();
     }
 
     public void StartNew()
@@ -42,6 +63,38 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    [System.Serializable]
+    class SaveData
+    {
+        public string name;
+        public int bestScore;
+    }
+
+    public void SaveGameInfo()
+    {
+        SaveData data = new SaveData();
+        data.name = name;
+        data.bestScore = bestScore;
+
+        string json = JsonUtility.ToJson(data);
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+     
+    }
+
+    public void LoadGameInfo()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            bestName = data.name;
+            bestScore = data.bestScore;
+        }
+    }
     public void Exit()
     {
 #if UNITY_EDITOR
