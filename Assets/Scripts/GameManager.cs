ff26f0095701fddcbd1094bf40edca48b22ffc28
Nibiru Imagineering - Lanny "gilbot" Gilbertson <lannygilbertson@protonmail.com>
2024-05-04 01:44:03 +0300

    Start Button Hookup - Basic
    - Hooked up Start Button functionality
    -- Calling StartNew() Method in GameManager script
    
    - Created StartNew() Method in GameManager Script
    
    - tested and currently works.
    -- if Player doesn't enter name and clicks Start Button they will get a Debug.Log console message that a name is required to play.
    -- if the player enters in a name in the Input field and clicks Start button they are taken to the main scene.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Text _bestScoreText;
+    [SerializeField] private InputField _playerName;
+
+    public string name = "";
+    public string bestName;
+    public int bestScore;
+
+    public static GameManager Instance;
+
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StartNew()
     {
-        
+        if (_playerName.text != "") 
+        {
+            name = _playerName.text;
+            SceneManager.LoadScene(1);
+        }
+        else
+        {
+            Debug.Log("Player Must Enter A Name To Play");
+        }
     }
 }
